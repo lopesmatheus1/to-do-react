4cdfe462207ebab576efe6c51f4d498165320ba5
Matheus Lopes Marendino <lopesmatheus1@hotmail.com>
2024-08-27 15:41:42 -0300

    chore:adding tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'], // Define os arquivos onde Tailwind CSS deve buscar classes CSS.
+  theme: {
+    extend: {}, // Este é o local onde você pode estender temas padrão do Tailwind CSS.
+  },
+  plugins: [], // Aqui você pode adicionar plugins para estender a funcionalidade do Tailwind CSS.
+}

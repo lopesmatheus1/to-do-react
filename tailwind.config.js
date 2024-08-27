e4ad95f6c71cfcb214967a44588b4d96fdbdda48
Matheus Lopes Marendino <lopesmatheus1@hotmail.com>
2024-08-27 16:10:35 -0300

    chore: add poppins font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'], // Define os arquivos onde Tailwind CSS deve buscar classes CSS.
   theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
     extend: {}, // Este é o local onde você pode estender temas padrão do Tailwind CSS.
   },
   plugins: [], // Aqui você pode adicionar plugins para estender a funcionalidade do Tailwind CSS.

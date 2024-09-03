42c95616442a8432c181b09f770d5a30c4dec9db
Matheus Lopes Marendino <lopesmatheus1@hotmail.com>
2024-09-03 18:29:27 -0300

    feat: adding InputErrorMessage component

---
+++ src/components/InputErrorMessage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import PropTypes from 'prop-types'
+
+const InputErrorMessage = ({ children }) => {
+  return <p className="text-left text-xs text-red-500">{children}</p>
+}
+
+InputErrorMessage.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default InputErrorMessage
--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+/* eslint-disable react/react-in-jsx-scope */
+import PropTypes from 'prop-types'
+import InputErrorMessage from './InputErrorMessage'
 import InputLabel from './InputLabel'
+import { forwardRef } from 'react'
 
-const TimeSelect = (props) => {
+const TimeSelect = forwardRef((props, ref) => {
   return (
     <div className="flex flex-col gap-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
       <select
         id="time"
         className="rounded-lg border border-solid border-brand-border px-4 py-3 outline-brand-primary placeholder:text-sm placeholder:text-brand-text-gray"
-        name="time"
+        ref={ref}
         {...props}
       >
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
         <option value="evening">Noite</option>
       </select>
+
+      {props.errorMessage && (
+        <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
+      )}
     </div>
   )
+})
+
+TimeSelect.displayName = 'TimeSelect'
+TimeSelect.propTypes = {
+  errorMessage: PropTypes.string,
 }
 
 export default TimeSelect

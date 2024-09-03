ecfbab86506f8ff79cf9d268c9a732e324d4c760
Matheus Lopes Marendino <lopesmatheus1@hotmail.com>
2024-09-03 18:28:37 -0300

    Feat: add react hook form to task-details page

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import { Right, ArrowLeft, TrashIcon, LoaderIcon } from '../assets/icons/index'
@@ -8,20 +8,22 @@ import Button from '../components/Button'
 import Input from '../components/Input'
 import TimeSelect from '../components/TimeSelect'
 import { toast } from 'sonner'
+import { useForm } from 'react-hook-form'
 
 /* eslint-disable react/react-in-jsx-scope */
 
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
   const [task, setTask] = useState()
-  const [saveIsLoading, setSaveIsLoading] = useState(false)
-  const [errors, setErrors] = useState([])
-
-  const titleRef = useRef()
-  const descriptionRef = useRef()
-  const timeRef = useRef()
-
   const navigate = useNavigate()
+
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    reset,
+  } = useForm()
+
   const handleBackClick = () => {
     navigate(-1)
   }
@@ -33,9 +35,10 @@ const TaskDetailsPage = () => {
       })
       const data = await response.json()
       setTask(data)
+      reset(data)
     }
     fetchTask()
-  }, [taskId])
+  }, [taskId, reset])
 
   const handleDeleteClick = async () => {
     const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
@@ -48,134 +51,125 @@ const TaskDetailsPage = () => {
     navigate(-1)
   }
 
-  const handleSaveClick = async () => {
-    setSaveIsLoading(true)
-    const newErros = []
-    const title = titleRef.current.value
-    const description = descriptionRef.current.value
-
-    if (!title.trim()) {
-      newErros.push({
-        inputName: 'title',
-        message: 'O título é obrigatório.',
-      })
-    }
-
-    if (!description.trim()) {
-      newErros.push({
-        inputName: 'description',
-        message: 'A descrição é obrigatório.',
-      })
-    }
-    setErrors(newErros)
-    if (newErros.length > 0) {
-      return setSaveIsLoading(false)
-    }
-
+  const handleSaveClick = async (data) => {
     const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
       method: 'PATCH',
       body: JSON.stringify({
-        title,
-        description,
+        title: data.title.trim(),
+        description: data.description.trim(),
+        time: data.time,
       }),
     })
     if (!response.ok) {
-      return setSaveIsLoading(false)
+      return toast.error('Ocorreu um erro ao salvar a tarefa')
     }
     const newTask = await response.json()
     setTask(newTask)
-    setSaveIsLoading(false)
+
     toast.success('Tarefa salva com sucesso')
   }
-
-  const titleError = errors.find((error) => error.inputName === 'title')
-  const timeError = errors.find((error) => error.inputName === 'time')
-  const descriptionError = errors.find(
-    (error) => error.inputName === 'description'
-  )
 
   return (
     <div className="flex">
       <Sidebar />
 
-      <div className="w-full space-y-6 px-8 py-16">
-        {/* BARRA TOP */}
-        <div className="flex w-full justify-between">
-          {/* PARTE DA ESQUERDA */}
-          <div>
-            <button
-              onClick={handleBackClick}
-              className="mb-3 flex h-8 w-8 items-center justify-center rounded-full bg-brand-primary"
-            >
-              <ArrowLeft />
-            </button>
-            <div className="flex items-center gap-1 text-xs">
-              <Link className="cursor-pointer text-brand-text-gray" to="/">
-                Minhas tarefas
-              </Link>
-              <Right className="text-brand-text-gray" />
-              <span className="font-semibold text-brand-primary">
+      <div className="w-full px-8 py-16">
+        <form className="space-y-6" onSubmit={handleSubmit(handleSaveClick)}>
+          {/* BARRA TOP */}
+          <div className="flex w-full justify-between">
+            {/* PARTE DA ESQUERDA */}
+            <div>
+              <button
+                onClick={handleBackClick}
+                className="mb-3 flex h-8 w-8 items-center justify-center rounded-full bg-brand-primary"
+              >
+                <ArrowLeft />
+              </button>
+              <div className="flex items-center gap-1 text-xs">
+                <Link className="cursor-pointer text-brand-text-gray" to="/">
+                  Minhas tarefas
+                </Link>
+                <Right className="text-brand-text-gray" />
+                <span className="font-semibold text-brand-primary">
+                  {task?.title}
+                </span>
+              </div>
+              <h1 className="mt-2 text-xl font-semibold text-brand-dark-blue">
                 {task?.title}
-              </span>
+              </h1>
             </div>
-            <h1 className="mt-2 text-xl font-semibold text-brand-dark-blue">
-              {task?.title}
-            </h1>
+            {/* PARTE DA DIREITA */}
+            <Button
+              onClick={handleDeleteClick}
+              className="h-fit self-end"
+              color="danger"
+            >
+              <TrashIcon />
+              Deletar Tarefa
+            </Button>
           </div>
+          <div className="space-y-6 rounded-xl bg-brand-white p-6">
+            <div>
+              <Input
+                id="title"
+                label="Título"
+                {...register('title', {
+                  required: 'O título é obrigatório.',
+                  validate: (value) => {
+                    if (!value.trim()) {
+                      return toast('O título é obrigatório.')
+                    }
+                    return true
+                  },
+                })}
+                errorMessage={errors?.title?.message}
+              />
+            </div>
 
-          {/* PARTE DA DIREITA */}
-          <Button
-            onClick={handleDeleteClick}
-            className="h-fit self-end"
-            color="danger"
-          >
-            <TrashIcon />
-            Deletar Tarefa
-          </Button>
-        </div>
-
-        <div className="space-y-6 rounded-xl bg-brand-white p-6">
-          <div>
-            <Input
-              id="title"
-              label="Título"
-              defaultValue={task?.title}
-              errorMessage={titleError?.message}
-              ref={titleRef}
-            />
+            <div>
+              <TimeSelect
+                {...register('time', {
+                  required: 'O horário é obrigatório',
+                  validate: (value) => {
+                    if (!value.trim()) {
+                      return toast('O horário é obrigatório.')
+                    }
+                    return true
+                  },
+                })}
+                errorMessage={errors?.time?.message}
+              ></TimeSelect>
+            </div>
+            <div>
+              <Input
+                id="description"
+                label="Descrição"
+                {...register('description', {
+                  required: 'A descrição é obrigatória.',
+                  validate: (value) => {
+                    if (!value.trim()) {
+                      return toast('A descrição é obrigatório.')
+                    }
+                    return true
+                  },
+                })}
+                errorMessage={errors?.description?.message}
+              />
+            </div>
           </div>
-
-          <div>
-            <TimeSelect
-              defaultValue={task?.time}
-              ref={timeRef}
-              errorMessage={timeError?.message}
-            ></TimeSelect>
+          <div className="flex w-full justify-end gap-3">
+            <Button
+              color="primary"
+              size="large"
+              className="bg-opacity-30"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting && <LoaderIcon className="animate-spin" />}
+              Salvar
+            </Button>
           </div>
-
-          <div>
-            <Input
-              id="description"
-              label="Descrição"
-              defaultValue={task?.description}
-              errorMessage={descriptionError?.message}
-              ref={descriptionRef}
-            />
-          </div>
-        </div>
-
-        <div className="flex w-full justify-end gap-3">
-          <Button
-            color="primary"
-            size="large"
-            className="bg-opacity-30"
-            onClick={handleSaveClick}
-            disabled={saveIsLoading}
-          >
-            {saveIsLoading && <LoaderIcon className="animate-spin" />}
-            Salvar
-          </Button>
-        </div>
+        </form>
       </div>
     </div>
   )
